1038c8b70c4659cde46a74674766d1e450d7db27
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2024-07-21 11:13:22 +0400

    Add game manager data autoloader

--- scripts/UI/UI.gd
+++ scripts/UI/UI.gd
@@ -13,11 +13,27 @@ signal deployable_passed(deployable : DeployableResource)
 @export var goat_resource : Growable
 @export var camel_resource : Growable
 
+@onready var calendar_text: Label = %CalendarText
+@onready var coin_text: Label = %CoinText
+@onready var wood_text: Label = %WoodText
+@onready var stone_text: Label = %StoneText
+@onready var progress_bar: ProgressBar = %ProgressBar
+@onready var water_text: Label = %WaterText
+
 func _ready() -> void:
 	barley_button.pressed.connect(handle_action_bar_press.bind(barley_button))
 	dates_button.pressed.connect(handle_action_bar_press.bind(dates_button))
 	goat_button.pressed.connect(handle_action_bar_press.bind(goat_button))
 	camel_button.pressed.connect(handle_action_bar_press.bind(camel_button))
+	
+	calendar_text.text = str(DataManager.calendar)
+	coin_text.text = str(DataManager.coins)
+	wood_text.text = str(DataManager.wood)
+	stone_text.text = str(DataManager.stone)
+	water_text.text = str(DataManager.water) + " / 10"
+	progress_bar.value = DataManager.water
+	
+	
 
 func handle_action_bar_press(button_pressed) -> void:
 	match button_pressed:
---
+++ scripts/globals/data_manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var calendar : int = 1 :
+	set(new_value):
+		calendar = new_value
+var coins : int = 10 :
+	set(new_value):
+		coins = new_value
+var water : int = 10 :
+	set(new_value):
+		water = new_value
+var wood : int = 0 :
+	set(new_value):
+		wood = new_value
+var stone : int = 0 :
+	set(new_value):
+		stone = new_value
