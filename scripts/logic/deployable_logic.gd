446f6bcb494fc6e8824f5e66a2695d507c907b18
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2024-07-21 22:12:11 +0400

    crops now grow

--- scripts/logic/deployable_logic.gd
+++ scripts/logic/deployable_logic.gd
@@ -2,11 +2,12 @@ extends Area2D
 
 @export var descriptor_text: Label
 @export var descriptor_ui: Control
-
 @export var resource: DeployableResource
+
 
 var has_built = false
 var first_click = true
+var current_sprite_index : int = 1
 
 func _ready() -> void:
 	descriptor_text.text = resource.descriptor_text
@@ -25,4 +26,22 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 		if first_click:
 			first_click = false
 		else:
-			resource.action()
+			advance_sprite()
+
+func advance_sprite():
+	if current_sprite_index < resource.growth_time:
+		hide_sprite(current_sprite_index)
+		current_sprite_index += 1
+		show_sprite(current_sprite_index)
+		if DataManager.water > 0:
+			DataManager.water -= 1
+		
+func hide_sprite(index):
+	var node = find_child(str(index))
+	print(node)
+	node.hide()
+
+func show_sprite(index):
+	var node = find_child(str(index))
+	print("second" + str(node))
+	node.show()
