1a7d43770022209315735ec0989c04da02966e4b
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2024-08-01 23:09:58 +0400

    progress with resource manager

--- scripts/manager_scripts/building_manager.gd
+++ scripts/manager_scripts/building_manager.gd
@@ -1,14 +1,47 @@
 extends ShapeCast2D
 
+signal buildable_state_changed(can_build: bool)
+
+var resource_type := ""
+
 func _process(delta: float) -> void:
 	if is_colliding():
-		var collider = get_collider(0)
-		if collider:
-			if collider is Plant:
-				print("Colliding with a plant")
-			else:
-				print("Colliding with an instance of ", collider.get_class())
-		else:
-			print("No collider found")
+		var collision_count = get_collision_count()
+		for i in range(collision_count):
+			var collider = get_collider(i)
+			if collider:
+				var can_build = true
+				match resource_type:
+					"animal":
+						can_build = check_animal_build_conditions(collider)
+					"plant":
+						can_build = check_plant_build_conditions(collider)
+					"material":
+						can_build = check_material_build_conditions(collider)
+					"building":
+						can_build = check_building_build_conditions(collider)
+					"free_tile":
+						can_build = check_free_tile_build_conditions(collider)
+					_:
+						can_build = false
+				buildable_state_changed.emit(can_build)
+				return
+		#buildable_state_changed.emit(false)
 	else:
-		print("Not colliding")
+		buildable_state_changed.emit(false)
+
+func check_animal_build_conditions(collider):
+	pass
+	
+func check_plant_build_conditions(collider):
+	pass
+
+func check_material_build_conditions(collider):
+	pass
+	
+func check_building_build_conditions(collider):
+	pass
+	
+func check_free_tile_build_conditions(collider):
+	pass
+	
--- scripts/manager_scripts/preview_manager.gd
+++ scripts/manager_scripts/preview_manager.gd
@@ -16,6 +16,9 @@ var can_build := false
 @onready var buildable_manager: ShapeCast2D = %BuildableManager
 @onready var preview_sprite: Sprite2D = $PreviewSprite
 
+func _ready() -> void:
+	buildable_manager.buildable_state_changed.connect(_on_buildable_state_changed)
+
 func _process(_delta: float) -> void:
 	if preview_active:
 		mouse_position = get_global_mouse_position()
@@ -29,25 +32,13 @@ func preview(deployable: DeployableResource):
 		preview_active = true
 	buildable_manager.show()
 	buildable_manager.enabled = true
+	buildable_manager.resource_type = deployable.resource_type
 	preview_sprite.scale = deployable.sprite_scale
 	preview_sprite.texture = deployable.sprite
-
-func preview_is_on_foundation(cell_position: Vector2i) -> bool:
-	if not preview_active:
-		return false
-	for cell in get_all_surrounding_cells(cell_position):
-		var tile_data = tile_map.get_cell_tile_data(2, cell)
-		if tile_data is TileData:
-			var buildable = tile_data.get_custom_data_by_layer_id(0)
-			if not buildable:
-				return false
-		else:
-			return false
-	return true
 	
 func _draw():
 	if preview_active:
-		if preview_is_on_foundation(tile_map_position):
+		if can_build:
 			draw_rect(building_area, Color(0, 1, 0, 0.5)) # Green
 		else:
 			draw_rect(building_area, Color(1, 0, 0, 0.5)) # Red
@@ -57,13 +48,8 @@ func _input(event) -> void:
 		preview_sprite.texture = null
 		buildable_manager.hide()
 		buildable_manager.enabled = false
+		buildable_manager.resource_type = ""
 		preview_active = false
 
-func get_all_surrounding_cells(middle_cell: Vector2i) -> Array:
-	var surrounding_cells: Array[Vector2i] = []
-	for y in range(3):
-		for x in range(3):
-			var target_cell : Vector2i = middle_cell + Vector2i(x - 1, y - 1)
-			if target_cell != middle_cell:
-				surrounding_cells.append(target_cell)
-	return surrounding_cells
+func _on_buildable_state_changed(check_can_build: bool) -> void:
+	can_build = check_can_build
