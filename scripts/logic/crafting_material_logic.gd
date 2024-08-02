afa010c9ccd35386d67a35f10319c08ece918d37
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2024-08-02 18:19:22 +0400

    Add wood and stone harvestable resources

--- scripts/logic/crafting_material_logic.gd
+++ scripts/logic/crafting_material_logic.gd
@@ -1,11 +1,19 @@
 extends Deployable
 class_name CraftingMaterial
 
-var first_click := true
-
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event.is_action_pressed("interact") && has_built:
-		if first_click:
-			first_click = false
-		else:
-			print("add crafting material action!")
+	if event.is_action_pressed("interact"):
+			consume_resource()
+			
+func consume_resource() -> void:
+	if DataManager.water > 0:
+		match resource.material_type:
+			"wood":
+				DataManager.wood += 1
+			"stone":
+				DataManager.stone += 1
+			"cloth":
+				DataManager.cloth += 1
+		if DataManager.water > 0:
+			DataManager.water -= 1
+		queue_free()
